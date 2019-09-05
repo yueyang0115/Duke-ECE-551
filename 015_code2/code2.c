4f3d6dd35c3c40876ffeab2862329922a8d5b74d
yy258 <yy258@duke.edu>
2019-09-05 15:56:49 -0400

    add lines in the file

--- 015_code2/code2.c
+++ 015_code2/code2.c
@@ -1,25 +1,24 @@
 
 int printTriangle(int size) {
-  //start with starCount being 0
+  int starCount = 0;  //start with starCount being 0
 
-  //count from 0 (inclusive) to size (exclusive), for each number i that you count
-
-     //count from 0 (inclusive) to i (inclusive), for each number j that you count
-
-        //print a "*"
-
-        //increment starCount
-
-     //when you finish counting on j, 
-
-     //print a newline ("\n")
-
-  //when you finish counting on i, 
-
+  for (int i = 0; i < size; i++) {
+    //count from 0 (inclusive) to size (exclusive), for each number i that you count
+    for (int j = 0; j <= i; j++) {
+      //count from 0 (inclusive) to i (inclusive), for each number j that you count
+      printf("*");
+      //print a "*"
+      starCount++;
+      //increment starCount
+    }
+    //when you finish counting on j,
+    printf("\n");
+    //print a newline ("\n")
+  }
+  //when you finish counting on i,
+  return starCount;
   //your answer is starCount
-
 }
-
 
 int main(void) {
   int numStars;
@@ -27,15 +26,12 @@ int main(void) {
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
   //now print "Here is a triangle with height 7\n"
-
+  printf("Here is a triangle with height 7\n");
   //then call printTriangle, passing in 7, and assign the result to numStars
-
-  //finally, print "That triangle had %d total stars\n", such that the %d 
+  numStars = printTriangle(7);
+  //finally, print "That triangle had %d total stars\n", such that the %d
+  printf("That triangle had %d total stars\n", numStars);
   //prints the value of numStars
-
 
   return 0;
 }
-
-
-
