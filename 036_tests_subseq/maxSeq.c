a14007e749287f89066f45ca075898e8cbb47ea2
yy258 <yy258@duke.edu>
2019-09-17 21:15:58 -0400

    make changes to maxSeq.cx

--- 036_tests_subseq/maxSeq.c
+++ 036_tests_subseq/maxSeq.c
@@ -1,20 +1,24 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 size_t maxSeq(int * array, size_t n) {
-  size_t maxSeq = 0;
-  if (n < 1) {
+  if (n <= 1) {
     return 0;
   }
-  if (n == 1) {
-    return 1;
-  }
+  size_t store_num[n];
+  size_t maxSeq = 0;
   for (size_t i = 0; i < n - 1; i++) {
     if (array[i] < array[i + 1]) {
       maxSeq++;
     }
+    store_num[i] = maxSeq + 1;
     maxSeq = 0;
   }
-  return maxSeq;
+  size_t max_number = store_num[0];
+  for (size_t i = 1; i < n; i++) {
+    if (store_num[i] > max_number) {
+      max_number = store_num[i];
+    }
+  }
+  return max_number;
 }
