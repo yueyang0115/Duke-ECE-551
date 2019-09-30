a6293137cafa6265df00ab64712d7a66b7e9728b
yy258 <yy258@duke.edu>
2019-09-29 23:48:46 -0400

    modify function split using strchr instead of strtok

--- 043_election/election.c
+++ 043_election/election.c
@@ -4,41 +4,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//include any other headers you need here...
 
-//Fuction split will split a string in dest by delim, and store the result in src
-void split(char * src, const char * delim, char ** dest, size_t * count) {
-  char * result = NULL;
-  result = strtok(src, delim);
-  while (result != NULL) {
-    *dest = result;
-    dest++;
-    (*count)++;
-    result = strtok(NULL, delim);
+/*Take in a string from src, split it by delimiter, and store elements in dest, the number of elements is count*/
+void split(const char * src, char delimiter, char dest[10][64], size_t * count) {
+  const char * p = src;
+  const char * pTmp = src;
+  const char * x = src;
+  const char * q = src;
+  size_t i = 0;
+
+  while (*p != '\0') {
+    p = strchr(p, delimiter);
+    if (p != NULL) {
+      strncpy(dest[i], pTmp, p - pTmp);
+      i++;
+      p++;
+      pTmp = p;
+    }
+    else {
+      break;
+    }
   }
+
+  x = strchr(src, '\0');
+  q = strrchr(src, delimiter);
+  if (q != NULL) {
+    strncpy(dest[i], q + 1, x - q - 1);  //find element after the last delimiter
+  }
+
+  *count = i + 1;  //calculate number of elements
 }
 
+/*Take in a string and return a state_t with information filled in*/
 state_t parseLine(const char * line) {
+  state_t state;
+  char delim = ':';
+  char parse_result[10][64] = {};
   size_t count = 0;
+
   if ((line == NULL) || (strlen(line) == 0)) {
     fprintf(stderr, "Line contains nothing\n");
     exit(EXIT_FAILURE);
   }
-  char line_cp[100] = {"\0"};
-  char * parse_result[10] = {"\0"};
-  strcpy(line_cp, line);
-  split(line_cp, ":", parse_result, &count);
-  if (count != 3) {
-    fprintf(stderr, "Line contains more than three elements\n");
-    exit(EXIT_FAILURE);
+  else {
+    split(line, delim, parse_result, &count);
+
+    if (count != 3) {
+      fprintf(stderr, "Line contains more or less than three elements\n");
+      exit(EXIT_FAILURE);
+    }
+    else {
+      strcpy(state.name, parse_result[0]);
+      state.population = atoi(parse_result[1]);
+      state.electoralVotes = atoi(parse_result[2]);
+    }
   }
-  state_t state;
-  strcpy(state.name, parse_result[0]);
-  state.population = atoi(parse_result[1]);
-  state.electoralVotes = atoi(parse_result[2]);
   return state;
 }
 
+/*Calculete the total number of received electoral votes*/
 unsigned int countElectoralVotes(state_t * stateData,
                                  uint64_t * voteCounts,
                                  size_t nStates) {
@@ -51,8 +75,10 @@ unsigned int countElectoralVotes(state_t * stateData,
   return count;
 }
 
+/*Prints out states that require a recount*/
 void printRecounts(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   float votePercentage[nStates];
+
   for (size_t i = 0; i < nStates; i++) {
     votePercentage[i] = 100 * voteCounts[i] / (float)stateData[i].population;
     if ((votePercentage[i] >= 49.5) && (votePercentage[i] <= 50.5)) {
@@ -63,10 +89,12 @@ void printRecounts(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   }
 }
 
+/*Find the max number in an array and return its index*/
 size_t max(float * array, size_t nStates) {
   float max_num = array[0];
   size_t index = 0;
-  for (size_t i = 0; i < nStates; i++) {
+
+  for (size_t i = 1; i < nStates; i++) {
     if (array[i] > max_num) {
       max_num = array[i];
       index = i;
@@ -75,12 +103,15 @@ size_t max(float * array, size_t nStates) {
   return index;
 }
 
+/*Find the state that Candidate A won the largest percentage of the vote*/
 void printLargestWin(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   float votePercentage[nStates];
+  size_t index = 0;
+
   for (size_t i = 0; i < nStates; i++) {
     votePercentage[i] = 100 * voteCounts[i] / (float)stateData[i].population;
   }
-  size_t index = max(votePercentage, nStates);
+  index = max(votePercentage, nStates);
   printf("Candidate A won %s with %.2f%% of the vote\n",
          stateData[index].name,
          votePercentage[index]);
