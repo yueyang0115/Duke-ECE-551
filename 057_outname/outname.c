c5d0d81b37bba9a9aa50f8e9a8251997ad7ff64f
yy258 <yy258@duke.edu>
2019-10-02 05:54:26 -0400

    add code

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,14 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  const char * suffix = ".counts";
+  unsigned len = strlen(inputName) + strlen(suffix) + 1;
+  char * outputName = malloc(len * sizeof(*outputName));
+  strcpy(outputName, inputName);
+  strcat(outputName, suffix);
+  return outputName;
 }
