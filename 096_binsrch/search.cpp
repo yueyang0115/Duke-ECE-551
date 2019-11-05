6994205701508e4ed87fa83cd906934f5894815b
yy258 <yy258@duke.edu>
2019-11-05 15:22:13 -0500

    change final return mid

--- 096_binsrch/search.cpp
+++ 096_binsrch/search.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <cstdlib>
 
 #include "function.h"
@@ -9,6 +10,44 @@ int binarySearchForZero(Function<int, int> * f, int low, int high) {
   if (low == high) {
     return low;
   }
+  if (f->invoke(low) >= 0) {
+    return low;
+  }
+  if (f->invoke(high - 1) <= 0) {
+    return high - 1;
+  }
+  high = high - 1;
+  int mid;
+  int guess;
+  while (low < high) {
+    mid = (low + high) / 2;
+    printf("mid=%d\n", mid);
+    guess = f->invoke(mid);
+    printf("f(mid)=%d\n", guess);
+    if (guess == 0) {
+      return mid;
+    }
+    if (guess > 0) {
+      high = mid - 1;
+      printf("high=%d\n", high);
+    }
+    else {
+      low = mid + 1;
+      printf("low=%d\n", low);
+    }
+  }
+  return mid;
+}
+
+/*
+int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  if (low > high) {
+    exit(EXIT_FAILURE);
+  }
+  if (low == high-1) {
+    return low;
+  }
+
   high = high - 1;
   int mid;
   int guess;
@@ -31,7 +70,8 @@ int binarySearchForZero(Function<int, int> * f, int low, int high) {
   if (guess > 0) {
     return low;
   }
-  else {
+  else{
     return mid;
   }
 }
+*/
