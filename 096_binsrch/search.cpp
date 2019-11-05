edc11a4c0256adf929b6df89913725547fd54464
yy258 <yy258@duke.edu>
2019-11-05 11:21:17 -0500

    change mid to guess, if guess<0 return high

--- 096_binsrch/search.cpp
+++ 096_binsrch/search.cpp
@@ -2,9 +2,10 @@
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
   high = high - 1;
   int mid;
+  int guess;
   while (low < high) {
     mid = (low + high) / 2;
-    int guess = f->invoke(mid);
+    guess = f->invoke(mid);
     if (guess == 0) {
       return 0;
     }
@@ -15,10 +16,10 @@ int binarySearchForZero(Function<int, int> * f, int low, int high) {
       low = mid + 1;
     }
   }
-  if (mid < 0) {
+  if (guess < 0) {
     return high;
   }
-  if (mid > 0) {
+  if (guess > 0) {
     return low;
   }
   else {
