95e7fa42500e62a83a268429954ae0bedbd94512
yy258 <yy258@duke.edu>
2019-11-09 14:18:23 -0500

    add code

--- 102_huff_tree/buildTree.cpp
+++ 102_huff_tree/buildTree.cpp
@@ -1,5 +1,27 @@
+#include <stdio.h>
+
+#include <cstdio>
+#include <cstdlib>
+
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() != 1) {
+    Node * mynodeone = pq.top();
+    pq.pop();
+    Node * mynodetwo = pq.top();
+    pq.pop();
+    Node * combinenode = new Node(mynodeone, mynodetwo);
+    pq.push(combinenode);
+  }
+  Node * root = pq.top();
+  pq.pop();
+  return root;
+}
